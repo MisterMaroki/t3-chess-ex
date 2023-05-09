0bfdda499e8982ed780d1f7ec7d7f02e742156fa
Omar Maroki <omar.maroki@cloderetailfinance.com>
2023-05-09 19:36:11 +0100

    fix rsc

--- src/app/(home)/home/addcs.ts
+++ src/app/(home)/home/addcs.ts
@@ -9,14 +9,20 @@ import { db } from '@/lib/db';
 
 export const validatedAction = zact(
 	z.object({
-		email: z.string().max(5),
+		email: z.string().email(),
 		userId: z.string(),
 	})
 )(async ({ email, userId }) => {
 	try {
+		const friendIds = (await fetchRedis(
+			'smembers',
+			`user:${userId}:friends`
+		)) as string[];
+		console.log('friend ids', friendIds);
+
 		const x = await db.get(`user:${userId}`);
 		console.log('🚀 ~ file: addcs.ts:23 ~ ) ~ x:', x);
-		return { message: `hello ${email}` };
+		return { message: `hello ${email}\n${friendIds}` };
 	} catch (error) {
 		console.log(error);
 		return { message: JSON.stringify(error) };
