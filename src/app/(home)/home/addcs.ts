e385b1f894f6e8eccdf8a7ea16991b6011726981
Omar Maroki <omar.maroki@cloderetailfinance.com>
2023-05-09 19:33:46 +0100

    fix server action

--- src/app/(home)/home/addcs.ts
+++ src/app/(home)/home/addcs.ts
@@ -14,17 +14,11 @@ export const validatedAction = zact(
 	})
 )(async ({ email, userId }) => {
 	try {
-		const friendIds = (await fetchRedis(
-			'smembers',
-			`user:${userId}:friends`
-		)) as string[];
-		console.log('friend ids', friendIds);
-
 		const x = await db.get(`user:${userId}`);
 		console.log('🚀 ~ file: addcs.ts:23 ~ ) ~ x:', x);
-		return { message: `hello ${email}\n${friendIds}` };
+		return { message: `hello ${email}` };
 	} catch (error) {
 		console.log(error);
-		return { message: 'error' };
+		return { message: JSON.stringify(error) };
 	}
 });
