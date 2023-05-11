7a8c56e1108ade380a7555ff675bc99e1ab24821
Omar Maroki <omar.maroki@cloderetailfinance.com>
2023-05-12 00:18:09 +0100

    plan setting up multiplayer games - db first

---
+++ src/app/(home)/chess/[game]/page.tsx
@@ -0,0 +1,24 @@
+import ChessGame from '@/components/chess/ChessGame';
+import { FC } from 'react';
+
+type Props = {
+	params: {
+		game: string;
+	};
+};
+
+const page: FC<Props> = ({ params }) => {
+	//check if game is in db
+	//if not, create new game
+	//if so, load game
+	//if game is over, show game over screen
+	//if game is not over, show game
+
+	return (
+		<div className="flex items-start justify-start w-full h-full gap-x-4">
+			<ChessGame />
+		</div>
+	);
+};
+
+export default page;
